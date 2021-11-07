3964b1326242c9d7fc10133e25937d70ab6e2640
Daniel Antal <antaldaniel@users.noreply.github.com>
2021-11-07 15:19:21 +0100

    test for reduce_dataset

---
+++ tests/testthat/test-reduce_dataset.R
@@ -0,0 +1,18 @@
+tr1 <- data.frame (
+  time = c(2010:2020),
+  AT = c(NA, 1:10),
+  BG = c(NA, NA, 1:6, NA, 8:9),
+  CZ = c(NA, 1:9, NA),
+  DK = c(rep(NA,10), 1),
+  EL = c(rep(NA, 11))
+)
+
+
+dfr <- reduce_dataset (df=tr1)
+
+test_that("reduction left no NA values", {
+  expect_true(sum(is.na(dfr))==0)
+})
+
+
+
